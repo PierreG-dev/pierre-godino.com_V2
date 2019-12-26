acb886f3f31bd29b111950fc33b416adc0492a95
AniTecK <aniteck82@hotmail.fr>
2019-12-27 00:32:40 +0100

    enhanced HTML mailing front-end

--- php/mail.php
+++ php/mail.php
@@ -8,17 +8,18 @@
 
 
     $to      = 'pierregodino.contact@yahoo.com';
-    $subject = 'DEVIS | ' . $name;
-    $message = ` <h1 style="text-align: center;margin-bottom: 50px">DEMANDE DE DEVIS</h1>
-                 <hr>
-                 <p style="text-align: center;">$content</p>
-                 <hr>
-                 <p style="text-align: center;">Numéro de téléphone: $phone</p>
-                 <p style="text-align: center;">Email: $mail</p>
-                `;
-    $headers = array(
-        'From' => 'site@pierre-godino.com',
-    );
+    $subject = $name;
+    $message = "<div style='background:#272727; padding: 50px; color: #ecf0f1;height: 600px;'>
+                    <h1 style='text-align: center;margin-bottom: 50px'>DEMANDE DE DEVIS</h1>
+                     <hr style='border-color: #c0392b'>
+                     <p style='text-align: center;font-size: 1.5rem;'>" . $content . "</p>
+                     <hr style='border-color: #c0392b'>
+                     <p style='text-align: center;'>Numero de telephone: " . $phone . "</p>
+                     <p style='text-align: center;'>Email: " . $mail . "</p>
+                </div>";
+    $headers .= 'From: DEVIS <site@pierre-godino.com>' . "\r\n"; 
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
     
     mail($to, $subject, $message, $headers);
